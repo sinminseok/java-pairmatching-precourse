cba7dabd9191221c3727b9b5c9d385437a85fdc5
sinminseok12 <sinminseok12@naver.com>
2024-12-11 21:01:11 +0900

    refactor(Level) : Level 필드 값 수정
    Level Enum 의 필드값인 misssion 을 제거했다.

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,21 +1,18 @@
 package pairmatching.domain;
 
 import java.util.Arrays;
-import java.util.List;
 
 public enum Level {
-    LEVEL1("레벨1", List.of("자동차 경주", "로또", "숫자야구게임")),
-    LEVEL2("레벨2", List.of("장바구니", "결제", "지하철노선도")),
-    LEVEL3("레벨3", List.of()),
-    LEVEL4("레벨4", List.of("성능개선", "배포")),
-    LEVEL5("레벨5", List.of());
+    LEVEL1("레벨1"),
+    LEVEL2("레벨2"),
+    LEVEL3("레벨3"),
+    LEVEL4("레벨4"),
+    LEVEL5("레벨5");
 
     private String name;
-    private List<String> missions;
 
-    Level(final String name, final List<String> missions) {
+    Level(final String name) {
         this.name = name;
-        this.missions = missions;
     }
 
     public static Level valueOfName(String name){
